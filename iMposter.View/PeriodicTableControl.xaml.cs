9146f7e00a77411edf5468cb03f040fb9b0d5ad9
Bartek Nowakowski <bnowakow@users.noreply.github.com>
2011-03-22 10:34:02 +0000

    First 3D periodic table model

--- iMposter.View/PeriodicTableControl.xaml.cs
+++ iMposter.View/PeriodicTableControl.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Media.Media3D;
 using iMposter.Model.Camera;
 using System.Windows.Threading;
 using iMposter.Controller.Face;
+using System.IO;
 
 namespace iMposter.View
 {
@@ -23,12 +24,19 @@ namespace iMposter.View
     /// </summary>
     public partial class PeriodicTableControl : UserControl
     {
+        protected bool[,] elementExists;
+        protected int columnNumber = 18;
+        protected int rowNumber = 7;
+        protected int elementWidth = 165;
+
         public PeriodicTableControl()
         {
             InitializeComponent();
+            InitializePeriodicTableElementsExistance();
+            InitializePeriodicTableElements();
 
-            this.MouseMove += new MouseEventHandler(PeriodicTableControl_MouseMove);
-            this.MouseWheel += new MouseWheelEventHandler(PeriodicTableControl_MouseWheel);
+            //this.MouseMove += new MouseEventHandler(PeriodicTableControl_MouseMove);
+            //this.MouseWheel += new MouseWheelEventHandler(PeriodicTableControl_MouseWheel);
         }
 
         void PeriodicTableControl_MouseWheel(object sender, MouseWheelEventArgs e)
@@ -44,5 +52,61 @@ namespace iMposter.View
         {
             camera.Position = new Point3D(10.0 - (e.GetPosition(this).X / this.ActualWidth * 10.0), e.GetPosition(this).Y / this.ActualHeight * 10.0, camera.Position.Z);
         }
+
+        protected void InitializePeriodicTableElements() {
+            for (int row = 0; row < rowNumber; row++) 
+            {
+                for (int column = 0; column < columnNumber; column++)
+                {
+                    if (elementExists[row, column])
+                    {
+                        TranslateTransform3D translate = new TranslateTransform3D(column * 2.5, row * -2.5, 0);
+                        Transform3DGroup tranformGroup = new Transform3DGroup();
+                        tranformGroup.Children.Add(translate);
+
+                        MeshGeometry3D mesh = new MeshGeometry3D();
+                        mesh.Positions = new Point3DCollection(new Point3D[] { new Point3D(-1, 1, 0), new Point3D(-1, -1, 0), new Point3D(1, -1, 0), new Point3D(1, 1, 0) });
+                        mesh.TextureCoordinates = new PointCollection(new Point[] { new Point(0, 0), new Point(0, 1), new Point(1, 1), new Point(1, 0) });
+                        mesh.TriangleIndices = new Int32Collection(new Int32[] { 0, 1, 2, 0, 2, 3 });
+
+                        DiffuseMaterial material = new DiffuseMaterial(Brushes.Gray);
+                        material.SetValue(Viewport2DVisual3D.IsVisualHostMaterialProperty, true);
+
+                        Image image = new Image();
+                        image.Width = elementWidth;
+                        image.Source = new BitmapImage(new Uri(@"Img\Elements\01_01.jpg", UriKind.Relative));
+
+                        Viewport2DVisual3D viewport = new Viewport2DVisual3D();
+                        viewport.Transform = tranformGroup;
+                        viewport.Geometry = mesh;
+                        viewport.Material = material;
+                        viewport.Visual = image;
+
+                        this.mainViewport.Children.Add(viewport);
+                    }
+                }
+            }
+        }
+
+        #region InitializePeriodicTableElementsExistance
+        private void InitializePeriodicTableElementsExistance()
+        {
+            elementExists = new bool[rowNumber, columnNumber];
+            for (int row = 0; row < rowNumber; row++) 
+            {
+                for (int column = 0; column < columnNumber; column++)
+                {
+                    if ((row == 0 && column > 0 && column < 17) || (row < 3 && column > 1 && column < 12))
+                    {
+                        elementExists[row, column] = false;
+                    }
+                    else
+                    {
+                        elementExists[row, column] = true;
+                    }
+                }
+            }
+        }
+        #endregion
     }
 }
