01a49991c9e89bce1ec0ff62dcc975e92208dcdd
Bartek Nowakowski <bnowakow@users.noreply.github.com>
2011-03-23 22:59:31 +0000

    Add camera preview

--- iMposter.View/CameraPreview.xaml.cs
+++ iMposter.View/CameraPreview.xaml.cs
@@ -23,16 +23,12 @@ namespace iMposter.View
     public partial class CameraPreview : UserControl
     {
         protected ICameraImage ci;
+        protected DispatcherTimer dt;
 
         public CameraPreview()
         {
             InitializeComponent();
-            ci = new CameraImage();
-            DispatcherTimer dt = new DispatcherTimer();
-            dt.Tick += new EventHandler(dt_Tick);
-            dt.Interval = TimeSpan.FromMilliseconds(500);
-            //dt.Start();
-            //dt_Tick(null, null);
+            
         }
 
         void dt_Tick(object sender, EventArgs e)
@@ -40,6 +36,16 @@ namespace iMposter.View
             // Need to add references for Emgu.CV and Emgu.Util in order to get extension method working
             BitmapSource bs = ci.GetNextImage().ToBitmapSource();
             cameraPreview.Source = bs;
+        }
+
+        private void captureButton_Click(object sender, RoutedEventArgs e)
+        {
+            ci = new CameraImage();
+            dt = new DispatcherTimer();
+            dt.Tick += new EventHandler(dt_Tick);
+            dt.Interval = TimeSpan.FromMilliseconds(500);
+            //dt_Tick(null, null);
+            dt.Start();
         }
     }
 }
